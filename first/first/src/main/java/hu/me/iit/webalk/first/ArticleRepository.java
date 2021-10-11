0d392df3f5d641b81a2b7531d88f7f31bf50f64a
Milliamper <szalaimarci99@gmail.com>
2021-10-11 15:08:52 +0200

    Október 11 gyakorlat

--- first/first/src/main/java/hu/me/iit/webalk/first/ArticleRepository.java
+++ first/first/src/main/java/hu/me/iit/webalk/first/ArticleRepository.java
@@ -3,12 +3,8 @@ package hu.me.iit.webalk.first;
 import java.util.List;
 
 public interface ArticleRepository {
-	
-	List<ArticleDto> findAll();
-
-	ArticleDto getById(Long id);
-
-	Long save(ArticleDto articleDto);
-
-	void deleteById(Long id);
+    List<ArticleDto> findAll();
+    ArticleDto getById(Long id);
+    Long save(ArticleDto articleDto);
+    void deleteById(Long id);
 }
