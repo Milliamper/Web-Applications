414b2ea12701255056f32ce31504644db7d36ca8
Milliamper <szalaimarci99@gmail.com>
2021-11-15 11:44:58 +0100

    November 15., hétfő gyakorlat

--- database/database/src/main/java/hu/iit/webalk/database/controller/PeopleController.java
+++ database/database/src/main/java/hu/iit/webalk/database/controller/PeopleController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import hu.iit.webalk.database.service.People;
@@ -31,32 +32,37 @@ public class PeopleController {
 	@GetMapping()
 	public Iterable<PeopleDTO> getAllPeople() {
 		List<PeopleDTO> peopleDtoList = new ArrayList<>();
-		
+
 		for (People people : peopleService.getAllPeople()) {
 			peopleDtoList.add(new PeopleDTO(people));
 		}
-		
+
 		return peopleDtoList;
 	}
-	
+
 	@PostMapping(consumes = "application/json")
 	public PeopleDTO save(@RequestBody @Valid PeopleCreateDTO peopleCreateDto) {
 		return new PeopleDTO(peopleService.create(peopleCreateDto.toPeople()));
 	}
-	
+
 	@DeleteMapping("/{id}") // ezzel jelölöm hogy az URL-ben hol van az a rész amit ki szeretnék venni
 	public void delete(@PathVariable("id") Long id) {
 		peopleService.delete(id);
 	}
-	
+
 	@GetMapping("/{id}")
 	public PeopleDTO getById(@PathVariable("id") Long id) {
 		return new PeopleDTO(peopleService.getById());
 	}
-	
+
 	@PutMapping
 	void put(@RequestBody @Valid PeopleDTO peopleDto) {
 		peopleService.save(peopleDto.toPeople());
+	}
+
+	@GetMapping("/findByAgeGt")
+	void findAdultPeople(@RequestParam("age") int age) {
+		peopleService.save(findByAgeGreaterThan(age));
 	}
 
 }
--- database/database/src/main/java/hu/iit/webalk/database/service/PeopleService.java
+++ database/database/src/main/java/hu/iit/webalk/database/service/PeopleService.java
@@ -1,15 +1,15 @@
 package hu.iit.webalk.database.service;
 
 public interface PeopleService {
-	
+
 	Iterable<People> getAllPeople();
 
 	void delete(Long id);
-	
+
 	People create(People toPeople);
-	
+
 	People getById(Long id);
-	
+
 	void save(People people);
 
 }
